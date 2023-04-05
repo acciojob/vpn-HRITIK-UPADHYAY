7c557596246fcf941f43f4c0527dda45b235a0f4
HRITIK UPADHYAY <hritikupadhyay134@gamil.com>
2023-04-05 17:37:58 +0530

    7 commit for vpn assignment

--- src/main/java/com/driver/services/impl/ConnectionServiceImpl.java
+++ src/main/java/com/driver/services/impl/ConnectionServiceImpl.java
@@ -21,14 +21,103 @@ public class ConnectionServiceImpl implements ConnectionService {
 
     @Override
     public User connect(int userId, String countryName) throws Exception{
+        User user = userRepository2.findById(userId).get();
+        if(user.getMaskedIp()!=null){
+            throw new Exception("Already connected");
+        }
+        else if(countryName.equalsIgnoreCase(user.getCountry().getCountryName().toString())){
+            return user;
+        }
+        else {
+            if (user.getServiceProviderList()==null){
+                throw new Exception("Unable to connect");
+            }
 
+            List<ServiceProvider> serviceProviderList = user.getServiceProviderList();
+            int a = Integer.MAX_VALUE;
+            ServiceProvider serviceProvider = null;
+            Country country =null;
+
+            for(ServiceProvider serviceProvider1:serviceProviderList){
+
+                List<Country> countryList = serviceProvider1.getCountryList();
+
+                for (Country country1: countryList){
+
+                    if(countryName.equalsIgnoreCase(country1.getCountryName().toString()) && a > serviceProvider1.getId() ){
+                        a=serviceProvider1.getId();
+                        serviceProvider=serviceProvider1;
+                        country=country1;
+                    }
+                }
+            }
+            if (serviceProvider!=null){
+                Connection connection = new Connection();
+                connection.setUser(user);
+                connection.setServiceProvider(serviceProvider);
+
+                String cc = country.getCode();
+                int givenId = serviceProvider.getId();
+                String mask = cc+"."+givenId+"."+userId;
+
+                user.setMaskedIp(mask);
+                user.setConnected(true);
+                user.getConnectionList().add(connection);
+
+                serviceProvider.getConnectionList().add(connection);
+
+                userRepository2.save(user);
+                serviceProviderRepository2.save(serviceProvider);
+
+
+            }
+        }
+        return user;
     }
     @Override
     public User disconnect(int userId) throws Exception {
+        User user = userRepository2.findById(userId).get();
+        if(!user.getConnected()) throw new Exception("Already disconnected");
 
+        user.setMaskedIp(null);
+        user.setConnected(Boolean.FALSE);
+        userRepository2.save(user);
+        return user;
     }
     @Override
     public User communicate(int senderId, int receiverId) throws Exception {
+        User receiver = userRepository2.findById(receiverId).get();
+        CountryName receiverCountryName = null;
+        if(receiver.getConnected()){
+            String maskedCode = receiver.getMaskedIp().substring(0,3);
+            if(maskedCode.equals("001")){
+                receiverCountryName = CountryName.IND;
+            }
+            else if(maskedCode.equals("002")){
+                receiverCountryName = CountryName.USA;
+            }
+            else if(maskedCode.equals("003")){
+                receiverCountryName = CountryName.AUS;
+            }
+            else if(maskedCode.equals("004")){
+                receiverCountryName = CountryName.CHI;
+            }
+            else if(maskedCode.equals("005")){
+                receiverCountryName = CountryName.JPN;
+            }
+            else {
+                receiverCountryName = receiver.getCountry().getCountryName();
+            }
+        }
 
+        User user = null;
+        try {
+            user = connect(senderId, receiverCountryName.toString());
+        }
+        catch (Exception e){
+            throw new Exception("Cannot establish communication");
+        }
+
+        return user;
     }
 }
